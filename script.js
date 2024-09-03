93e580c0a90d6c09046484f0744a48889fc0ae96
Chonkz90 <zahlerdal@yahoo.com>
2024-09-03 12:06:12 +0200

    wwesxdfcgvhbjkiolp

--- script.js
+++ script.js
@@ -26,8 +26,44 @@ console.log(movieListElement)
 for (const movie of data) {
     console.log(movie)
 
-    const movieCard = document.createElement("li")
+    const movieCard = createMovieCard(movie)
+    const poster = movieCard.querySelector
+
     movieCard.textContent = movie.title
 
     movieListElement.append(movieCard)
+}
+
+const templateString = `
+    <img>
+    <p class="title"></p>`
+
+function createMovieCard(movie){
+    const movieCard = document.createElement("li")
+    movieCard.className = "movie-card"
+
+    const titleElement = document.createElement("p")
+    titleElement.textContent = movie.title
+    titleElement.className = "title"
+
+    const posterElement = document.createElement("img")
+    posterElement.src = movie.image
+
+    movieCard.append(posterElement)
+    movieCard.append(titleElement)
+
+    movieCard.innerHTML= templateString
+
+    const poster = movieCard.querySelector("img")
+    poster.src= movie.image
+
+    const title = movieCard.querySelector(".title")
+    title.textContent = movie.title
+
+    
+    for (const people of movie.people) {
+
+    }
+
+    return movieCard
 }
